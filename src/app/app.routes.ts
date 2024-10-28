18563e535bdfe381132d37460c82f8cbf0f18b4f
“G-Gakii” <gakiiviolet1@gmail.com>
2024-10-28 13:40:03 +0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MycalculatorComponent } from './components/mycalculator/mycalculator.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MycalculatorComponent,
+  },
+];
---
+++ src/app/components/mycalculator/mycalculator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mycalculator',
+  standalone: true,
+  imports: [],
+  templateUrl: './mycalculator.component.html',
+  styleUrl: './mycalculator.component.css'
+})
+export class MycalculatorComponent {
+
+}
